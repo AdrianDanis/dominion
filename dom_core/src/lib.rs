89a4f0687332663764be3a8b379799c0ad1df8db
Adrian Danis <rambobones@gmail.com>
2018-08-02 15:17:07 +1000

    Start defining top level data structures

--- dom_core/src/lib.rs
+++ dom_core/src/lib.rs
@@ -1,3 +1,28 @@
+enum Mutation {
+}
+
+/// Definition of the state of a board
+///
+/// This structure is immutable and any mutations must be done through an explicit mutation
+struct BoardState {
+}
+
+impl BoardState {
+    fn mutate(self, m: Mutation) -> BoardState {
+        unimplemented!()
+    }
+}
+
+/// Defines and runs the rules and logic of a dominion game
+///
+/// Internally has a `BoardState` and 
+struct Game {
+}
+
+impl Game {
+    fn waiting_on(&self) -> 
+}
+
 #[cfg(test)]
 mod tests {
     #[test]
